baf80185810414b530775b260be97384c5c23d69
CalebProffitt <calebproffitt97@gmail.com>
2020-02-21 05:43:00 -0500

    Easy C#

--- CS_311_Project2/MainWindow.xaml.cs
+++ CS_311_Project2/MainWindow.xaml.cs
@@ -24,5 +24,103 @@ namespace CS_311_Project2
         {
             InitializeComponent();
         }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            int age = AgeValue(txtAge.Text);
+            int RR = RRValue(txtRR.Text);
+            int SBP = SBPValue(txtSBP.Text);
+            int pulse = PulseValue(txtPulse.Text);
+            int pH = Convert.ToInt32(PHValue(txtPH.Text));
+            int sodium = SodiumValue(txtSodium.Text);
+            int hema = HemaValue(txtHema.Text);
+
+            int intOutput = age + RR + SBP + pulse + pH + sodium + hema;
+            txtblkOutput.Text = Convert.ToString(intOutput);
+        }
+
+        static int AgeValue(string txtAge)
+        {
+            int age = Convert.ToInt32(txtAge);
+            return age;
+        }
+
+        static int RRValue(string txtRR)
+        {
+            int RR = Convert.ToInt32(txtRR);
+            if (RR >= 30)
+            {
+                return 20;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        static int SBPValue(string txtSBP)
+        {
+            int SBP = Convert.ToInt32(txtSBP);
+            if (SBP < 90)
+            {
+                return 20;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        static int PulseValue(string txtPulse)
+        {
+            int pulse = Convert.ToInt32(txtPulse);
+            if (pulse >= 125)
+            {
+                return 10;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        static double PHValue(string txtPH)
+        {
+            double pH = Convert.ToDouble(txtPH);
+            if (pH < 7.35)
+            {
+                return 30;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        static int SodiumValue(string txtSodium)
+        {
+            int sodium = Convert.ToInt32(txtSodium);
+            if (sodium < 130)
+            {
+                return 20;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        static int HemaValue(string txtHema)
+        {
+            int hema = Convert.ToInt32(txtHema);
+            if (hema < 30)
+            {
+                return 10;
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
 }
